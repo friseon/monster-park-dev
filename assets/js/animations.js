f04e325c692281ab80a1f178c635002707dd449c
Maximka <frise.on@gmail.com>
2020-03-18 20:28:29 +0300

    dancer

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -5,3 +5,11 @@ setInterval(() => {
     bigEye.style.backgroundPositionX = `${-330 * eyeCounter}px`;
     eyeCounter = (eyeCounter === 5 ? 0 : (eyeCounter + 1));
 }, 200);
+
+const danceMan = document.querySelector('.dance-man');
+let danceCounter = 0;
+
+setInterval(() => {
+    danceMan.style.backgroundPositionX = `${-250 * danceCounter}px`;
+    danceCounter = (danceCounter === 3 ? 0 : (danceCounter + 1));
+}, 300);
