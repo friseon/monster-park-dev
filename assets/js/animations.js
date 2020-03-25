1e0736709ffec2bafd7c500cf04778fae9239a82
Maximka <frise.on@gmail.com>
2020-03-26 00:19:19 +0300

    квест

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -1,3 +1,8 @@
+const questState = {
+    monumentRotate: false,
+    codeEnter: false
+}
+
 const bigEye = document.querySelector('.big-eye');
 let eyeCounter = 0;
 
@@ -38,6 +43,51 @@ setInterval(() => {
     fireCounter = (fireCounter === 2 ? 0 : (fireCounter + 1));
 }, 120);
 
+const dream = document.querySelector('.dream');
+let dreamCounter = 0;
+
+setInterval(() => {
+    dream.style.backgroundPositionX = `${-200 * dreamCounter}px`;
+    dreamCounter = (dreamCounter === 3 ? 0 : (dreamCounter + 1));
+}, 300);
+
+const stereo = document.querySelector('.stereo');
+let stereoCounter = 0;
+let steroInterval = null;
+
+const stereoPlay = () => {
+    steroInterval = setInterval(() => {
+        stereo.style.backgroundPositionX = `${-250 * stereoCounter}px`;
+        stereoCounter = (stereoCounter === 2 ? 0 : (stereoCounter + 1));
+    }, 300);
+};
+
+const stereoStop = () => {
+    clearInterval(steroInterval);
+    stereoCounter = 0;
+    stereo.style.backgroundPositionX = `${-250 * stereoCounter}px`;
+}
+
+const code = document.querySelector('.code');
+const codeInput = document.querySelector('.code-input');
+const king = document.querySelector('.king');
+let codeHasEnter = false;
+
+codeInput.addEventListener('input', (event) => {
+    if (!questState.codeEnter && Number(event.target.value) === 66613) {
+        questState.codeEnter = true;
+        stereoPlay();
+        king.classList.add('show');
+        dream.classList.add('hide');
+    } else if (questState.codeEnter) {
+        questState.codeEnter = false;
+        stereoStop();
+        king.classList.remove('show');
+        dream.classList.remove('hide');
+    }
+});
+
+
 const monument = document.querySelector('.monument');
 let monumentCounter = 1;
 // направление изменения кадров
@@ -49,6 +99,14 @@ monument.addEventListener('click', (event) => {
     event.stopPropagation();
 
     monument.style.backgroundPositionX = `${-240 * monumentCounter}px`;
+
+    if (monumentCounter === 3) {
+        questState.monumentRotate = true;
+        code.classList.add('show');
+    } else {
+        code.classList.remove('show');
+    }
+
     if (monumentCounter === 4 && diffMonumentCounter > 0 || monumentCounter === 0 && diffMonumentCounter < 0) {
         // если доходим на крайних кадров, то меняем направление изменения кадров
         diffMonumentCounter = -diffMonumentCounter;
